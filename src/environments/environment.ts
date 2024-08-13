c8e6422880b83f9c3d1f7367064774353db60d14
Daniel Holterhaus <daniel.holterhaus@gmail.com>
2024-08-13 14:03:02 +0200

    added firestore hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDkYLrE_RBvRLbRUphknCVjvztW0srCKo8",
+    authDomain: "obx-vacay.firebaseapp.com",
+    projectId: "obx-vacay",
+    storageBucket: "obx-vacay.appspot.com",
+    messagingSenderId: "39718923709",
+    appId: "1:39718923709:web:f02063c16a8548e891f6d4",
+    measurementId: "G-GZL3S571M2"
+  }
 };
 
 /*
